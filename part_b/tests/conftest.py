4929e8a1cee8535bb07120d93e54b30e807c8ca9
ThuanDanchoi <ducthuantrandc@gmail.com>
2025-05-19 12:51:18 +1000

    d
    d

--- part_b/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# part_b/tests/conftest.py
-import sys, os
-
-# Đưa root của repo (lên hai cấp) vào đầu sys.path
-ROOT = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
-if ROOT not in sys.path:
-    sys.path.insert(0, ROOT)
